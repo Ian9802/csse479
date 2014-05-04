932a9e05e490034dc12a5fc0a3e37833342a7c58
Ian <Obe_ty@yahoo.com>
2014-05-04 18:26:16 -0400

    AES fully functional, and commented

--- AES.py
+++ AES.py
@@ -1,25 +1,22 @@
 sBox = [
-    [ 99,124,119,123,242,107,111,197, 48,  1,103, 43,254,215,171,118],
-    [202,130,201,125,250, 89, 71,240,173,212,162,175,156,164,114,192],
-    [183,253,147, 38, 54, 63,247,204, 52,165,229,241,113,216, 49, 21],
-    [  4,199, 35,195, 24,150,  5,154,  7, 18,128,226,235, 39,178,117],
-    [  9,131, 44, 26, 27,110, 90,160, 82, 59,214,179, 41,227, 47,132],
-    [ 83,209,  0,237, 32,252,177, 91,106,203,190, 57, 74, 76, 88,207],
-    [208,239,170,251, 67, 77, 51,133, 69,249,  2,127, 80, 60,159,168],
-    [ 81,163, 64,143,146,157, 56,245,188,182,218, 33, 16,255,243,210],
-    [205, 12, 19,236, 95,151, 68, 23,196,167,126, 61,100, 93, 25,115],
-    [ 96,129, 79,220, 34, 42,144,136, 70,238,184, 20,222, 94, 11,219],
-    [224, 50, 58, 10, 73,  6, 36, 92,194,211,172, 98,145,149,228,121],
-    [231,200, 55,109,141,213, 78,169,108, 86,244,234,101,122,174,  8],
-    [186,120, 37, 46, 28,166,180,198,232,221,116, 31, 75,189,139,138],
-    [112, 62,181,102, 72,  3,246, 14, 97, 53, 87,185,134,193, 29,158],
-    [225,248,152, 17,105,217,142,148,155, 30,135,233,206, 85, 40,223],
-    [140,161,137, 13,191,230, 66,104, 65,153, 45, 15,176, 84,187, 22]
+    [ 99, 124, 119, 123, 242, 107, 111, 197,  48,   1, 103,  43, 254, 215, 171, 118],
+    [202, 130, 201, 125, 250,  89,  71, 240, 173, 212, 162, 175, 156, 164, 114, 192],
+    [183, 253, 147,  38,  54,  63, 247, 204,  52, 165, 229, 241, 113, 216,  49,  21],
+    [  4, 199,  35, 195,  24, 150,   5, 154,   7,  18, 128, 226, 235,  39, 178, 117],
+    [  9, 131,  44,  26,  27, 110,  90, 160,  82,  59, 214, 179,  41, 227,  47, 132],
+    [ 83, 209,   0, 237,  32, 252, 177,  91, 106, 203, 190,  57,  74,  76,  88, 207],
+    [208, 239, 170, 251,  67,  77,  51, 133,  69, 249,   2, 127,  80,  60, 159, 168],
+    [ 81, 163,  64, 143, 146, 157,  56, 245, 188, 182, 218,  33,  16, 255, 243, 210],
+    [205,  12,  19, 236,  95, 151,  68,  23, 196, 167, 126,  61, 100,  93,  25, 115],
+    [ 96, 129,  79, 220,  34,  42, 144, 136,  70, 238, 184,  20, 222,  94,  11, 219],
+    [224,  50,  58,  10,  73,   6,  36,  92, 194, 211, 172,  98, 145, 149, 228, 121],
+    [231, 200,  55, 109, 141, 213,  78, 169, 108,  86, 244, 234, 101, 122, 174,   8],
+    [186, 120,  37,  46,  28, 166, 180, 198, 232, 221, 116,  31,  75, 189, 139, 138],
+    [112,  62, 181, 102,  72,   3, 246,  14,  97,  53,  87, 185, 134, 193,  29, 158],
+    [225, 248, 152,  17, 105, 217, 142, 148, 155,  30, 135, 233, 206,  85,  40, 223],
+    [140, 161, 137,  13, 191, 230,  66, 104,  65, 153,  45,  15, 176,  84, 187,  22]
 ]
 
-
-def con(x):
-    return '{0:08b}'.format(x)
 
 cdKey = [
     [2, 3, 1, 1],
@@ -39,6 +36,7 @@ def main():
 
 
 def outData(data):
+    # converts from array of 'matrices' or integers to an array of binary strings
     outValue = list()
     for message in data:
         outString = ""
@@ -52,10 +50,12 @@ def outData(data):
 def execution(key, data):
     outData = list()
     for row in data:
+        #converts from binary to matrix
         base = convert(start(row))
         keyBox = convert(keystart(key))
         tKey = transpose(keyBox)
-        #0
+        #begin AES
+        #stage 0
         aBox = ark(base, tKey)
         for x in range(0, 9):
             bBox = bs(aBox)
@@ -64,39 +64,28 @@ def execution(key, data):
             keyBox = nkb(keyBox, x)
             tKey = transpose(keyBox)
             aBox = ark(dBox, tKey)
-        #10
+        #stage 10
         bBox = bs(aBox)
         cBox = sr(bBox)
         keyBox = nkb(keyBox, 9)
         tKey = transpose(keyBox)
         aBox = ark(cBox, tKey)
+        #end AES
         outData.append(transpose(aBox))
     return outData
 
 
-def hexBox(matrix):
-    box = [[] for foo in range(0, 4)]
-    for x in range(0, 4):
-        for y in range(0, 4):
-            box[x].append(hex(matrix[x][y]))
-    return box
-
-
 def convert(inValue):
+    #builds array of ints from array of strings of binary
     outValue = [[] for foo in range(0, 4)]
     for x in range(0, 4):
         for y in range(0, 4):
             outValue[x].append(int(inValue[x][y], 2))
     return outValue
 
-# def stringToBin(string):
-#     binString = list()
-#     for x in range(0, 16):
-#         binString.append('{0:08b}'.format(ord(string[x])))
-#     return binString
-
 
 def start(binString):
+    #builds column indexed array of binary strings
     aBox = [[] for foo in range(0, 4)]
     for x in range(0, 4):
         for y in range(0, 4):
@@ -105,6 +94,10 @@ def start(binString):
 
 
 def keystart(binString):
+    #build row indexed array of binary strings
+    #this is here because all of the key functions were already written in row index
+    #transpose flips things back and forth so they match with the functions and
+    #I don't have to re-re-re-re-rewrite the key functions.
     keyBox = [[] for foo in range(0, 4)]
     for x in range(0, 4):
         for y in range(0, 4):
@@ -113,14 +106,18 @@ def keystart(binString):
 
 
 def bs(aBox):
+    #byte substitution, uses the sBox matrix at the top
+    #matrix here
     bBox = [[] for foo in range(0, 4)]
     for x in range(0, 4):
         for y in range(0, 4):
+            #value here
             bBox[x].append(indBS(aBox[x][y]))
     return bBox
 
 
 def indBS(b):
+    #finds the value for int input in the sBox
     val = '{0:08b}'.format(b)
     row = val[:4]
     col = val[4:]
@@ -130,32 +127,42 @@ def indBS(b):
 
 
 def sr(bBox):
+    #shift row on the input matrix
+    #matrix here
     cBox = [[] for foo in range(0, 4)]
     for x in range(0, 4):
         for y in range(0, 4):
+            #value here
             cBox[x].append(bBox[x][(y+x) % 4])
     return cBox
 
 
 def mc(cBox):
+    #mix column transformation
+    #matrix here
     dBox = [[] for foo in range(0, 4)]
     tcBox = transpose(cBox)
     for x in range(0, 4):
+        #column here
         dBox[x].extend(mux(tcBox[x]))
     return transpose(dBox)
 
 
 def mux(col):
+    #does the individual mix column transformation for the columns themselves
     column = list()
     for x in range(0, 4):
         value = 0
         for y in range(0, 4):
-            #might be how this works?
+            #this was a pain to get right
             value = value ^ (finiteMult(cdKey[x][y], col[y]))
         column.append(value)
     return column
 
 def finiteMult(a, b):
+    #does multiplication in a finite field, built from the guide at:
+    #http://en.wikipedia.org/wiki/Finite_field_arithmetic
+    #(with a little bit of cleverness and magic)
     aString = '{0:08b}'.format(a)
     bString = '{0:08b}'.format(b)
     p = 0
@@ -172,6 +179,7 @@ def finiteMult(a, b):
     return p
 
 def transpose(matrix):
+    #transposes a matrix
     box = [[] for foo in range(0, 4)]
     for x in range(0, 4):
         for y in range(0, 4):
@@ -179,51 +187,72 @@ def transpose(matrix):
     return box
 
 def ark(dBox, keyBox):
+    #Add Round Key, does the round key addition for matrices dBox and keyBox
+    #matrix here
     eBox = [[] for foo in range(0, 4)]
     for x in range(0, 4):
         for y in range(0, 4):
+            #value here
             val = dBox[x][y] ^ keyBox[x][y]
             eBox[x].append(val)
     return eBox
 
 
 def nkb(oldKeyBox, i):
+    #New Key Box, follows the key schedule rules to produce the next key box.
+    #matrix here
     keyBox = [[] for foo in range(0, 4)]
     for x in range(0, 4):
+        #column here
         if x % 4 == 0:
+            #4|i
             keyBox[x].extend(wFun(oldKeyBox[x], tFun(oldKeyBox[3], (i+1)*4)))
         else:
+            #4∤i
             keyBox[x].extend(wFun(oldKeyBox[x], keyBox[(x-1)]))
     return keyBox
 
 
 def wFun(col1, col2):
+    #W function, xors the input columns
+    #column here
     column = list()
     for x in range(0, 4):
+        #value here
         column.append(col1[x] ^ col2[x])
     return column
 
 
 def tFun(oldColumn, i):
+    #T function, generates the T column
+    #column here
     tempColumn = list()
     returnColumn = list()
     for x in range(0, 4):
+        #rotates column up and stores in temp
         tempColumn.append(indBS(oldColumn[(x+1) % 4]))
+    #round constant
     c = roundC(i)
+    #produces e xor r(i)
     returnColumn.append(c ^ tempColumn[0])
     for x in range(1, 4):
+        #stores rest of column
         returnColumn.append(tempColumn[x])
+    #not the most space nor time efficient function, but it works and is sturdy.
     return returnColumn
 
 
 def roundC(val):
+    #comuptes the round constant off of the value input
     out = 1
     for x in range(0, (val-4)//4):
+        #more finite field multiplication!
         out = finiteMult(out, 2)
     return out
 
 
 def readDoc():
+    #reads in the document
     file = open("aesinput.txt", 'r')
     data = list()
     for line in file:
@@ -233,6 +262,7 @@ def readDoc():
 
 
 def writeDoc(data):
+    #writes out the document
     file = open("aesianhallamout.txt", 'w')
     for message in data:
         file.write(message)
